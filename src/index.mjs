d637c5a988f772e75663a2ef616ec37fb3d5f1f1
CJ Lindblad <cj.lindblad@bitio.se>
2019-05-29 15:21:02 +0200

    Added keyboard input as well.

--- src/index.mjs
+++ src/index.mjs
@@ -5,19 +5,79 @@ function main() {
   setupInputListeners(html);
 }
 
+// temporary way of testing input
 function setLabelText(text) {
   const label = document.getElementById('input');
   label.innerText = text;
+}
+
+// input types
+const INPUT_LEFT = 'INPUT_LEFT';
+const INPUT_RIGHT = 'INPUT_RIGHT';
+const INPUT_UP = 'INPUT_UP';
+const INPUT_DOWN = 'INPUT_DOWN';
+const INPUT_MAIN_ACTION = 'INPUT_MAIN_ACTION';
+
+function registerInput(input) {
+  switch (input) {
+    case INPUT_LEFT:
+      setLabelText('left');
+      break;
+    case INPUT_RIGHT:
+      setLabelText('right');
+      break;
+    case INPUT_UP:
+      setLabelText('up');
+      break;
+    case INPUT_DOWN:
+      setLabelText('down');
+      break;
+    case INPUT_MAIN_ACTION:
+      setLabelText('🎉');
+      break;
+    default:
+      throw new Error(`Unknown input - ${input}`)
+      break;
+  }
 }
 
 function setupInputListeners(element) {
   // will probably break with multi touch.
   let touches = [];
 
+  // swipe listeners
   element.addEventListener('touchstart', handleTouchStart, false);
   element.addEventListener('touchend', handleTouchEnd, false);
   element.addEventListener('touchcancel', handleTouchCancel, false);
   element.addEventListener('touchmove', handleTouchMove, false);
+
+  // why not key listeners as well?
+  element.addEventListener('keydown', handleKeyDown, false);
+
+  function handleKeyDown(event) {
+    // some key codes
+    const LEFT = 37;
+    const RIGHT = 39;
+    const UP = 38;
+    const DOWN = 40;
+    const SPACE = 32;
+
+    if (event.keyCode === LEFT) {
+      registerInput(INPUT_LEFT);
+    }
+    if (event.keyCode === RIGHT) {
+      registerInput(INPUT_RIGHT);
+    }
+    if (event.keyCode === UP) {
+      registerInput(INPUT_UP);
+    }
+    if (event.keyCode === DOWN) {
+      registerInput(INPUT_DOWN);
+    }
+    if (event.keyCode === SPACE) {
+      registerInput(INPUT_MAIN_ACTION);
+    }
+  }
 
   function handleTouchStart(event) {
     touches.push(event.touches[0]);
@@ -35,28 +95,29 @@ function setupInputListeners(element) {
     const dy = lastTouch.pageY - initialTouch.pageY;
 
     const inputType = getInputType(dx, dy);
-    setLabelText(inputType);
+    registerInput(inputType);
 
     touches = [];
   }
 
   function getInputType(dx, dy) {
     if (dx === 0 && dy === 0) {
-      return 'tap';
+      // tap
+      return INPUT_MAIN_ACTION;
     }
 
     const isHorizontal = Math.abs(dx) > Math.abs(dy);
     if (isHorizontal) {
       if (dx > 0) {
-        return 'right';
+        return INPUT_RIGHT;
       } else {
-        return 'left';
+        return INPUT_LEFT;
       }
     } else {
       if (dy > 0) {
-        return 'down';
+        return INPUT_DOWN;
       } else {
-        return 'up';
+        return INPUT_UP;
       }
     }
   }
