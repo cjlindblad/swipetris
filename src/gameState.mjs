f948bf9daf5bf1086c87b4f119b90ea57bc3b1d2
cj lindblad <carljohan.lindblad@gmail.com>
2019-05-30 23:21:13 +0200

    Added game state component

---
+++ src/gameState.mjs
@@ -0,0 +1,29 @@
+/*
+
+piece rotation would work like this
+
+.... ..x. .... .x..
+xxxx ..x. .... .x..
+.... ..x. xxxx .x..
+.... ..x. .... .x..
+
+xxx ..x ... x..
+.x. .xx .x. xx.
+... ..x xxx x..
+
+xx. ..x ... .x.
+.xx .xx xx. xx.
+... .x. .xx x..
+
+.xx .x. ... x.. 
+xx. .xx .xx xx.
+... ..x xx. .x.
+
+xx xx xx xx
+xx xx xx xx
+
+*/
+
+export const initializeGameState = () => {
+  // TODO
+};
--- src/index.mjs
+++ src/index.mjs
@@ -1,8 +1,12 @@
 import { setupInputListeners } from './inputHandling.mjs';
 
+import { initializeGameState } from './gameState.mjs';
+
 function main() {
   const html = document.getElementById('wrapper');
   setupInputListeners(html);
+
+  initializeGameState();
 }
 
 // startup
