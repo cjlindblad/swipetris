c1d6cbd71be1c5e45639651ecb2133476d09397f
cj lindblad <carljohan.lindblad@gmail.com>
2019-08-10 21:15:07 +0200

    Added initial info window function to terminal

--- src/render/terminal.ts
+++ src/render/terminal.ts
@@ -1,3 +1,4 @@
+// TODO break this up
 const render = (param: RenderParam): void => {
   const { renderString, nextPiece, score } = param;
 
@@ -25,12 +26,79 @@ const render = (param: RenderParam): void => {
 
   const expandedRenderString = expandSize(renderString, 4, 2);
 
+  const addInfoWindow = (renderString: string): string => {
+    const lines = renderString.split('\n');
+    let longestLineLength = 0;
+    lines.forEach(line => {
+      if (line.length > longestLineLength) {
+        longestLineLength = line.length;
+      }
+    });
+
+    const MODAL_WIDTH = 20;
+    const MODAL_HEIGHT = 5;
+    const MODAL_X_OFFSET = longestLineLength / 2 - MODAL_WIDTH / 2;
+    const MODAL_Y_OFFSET = 10;
+
+    const result = lines.map((line, index) => {
+      const isFirstOrLastLine =
+        index === MODAL_Y_OFFSET || index === MODAL_Y_OFFSET + MODAL_HEIGHT - 1;
+      if (isFirstOrLastLine) {
+        return line
+          .split('')
+          .map((c, i) => {
+            if (
+              i === MODAL_X_OFFSET ||
+              i === MODAL_X_OFFSET + MODAL_WIDTH - 1
+            ) {
+              return '+';
+            } else if (
+              i >= MODAL_X_OFFSET &&
+              i < MODAL_X_OFFSET + MODAL_WIDTH
+            ) {
+              return '-';
+            }
+            return c;
+          })
+          .join('');
+      }
+
+      const isInBetweenLine =
+        index > MODAL_Y_OFFSET && index < MODAL_Y_OFFSET + MODAL_HEIGHT - 1;
+      if (isInBetweenLine) {
+        return line
+          .split('')
+          .map((c, i) => {
+            if (
+              i === MODAL_X_OFFSET ||
+              i === MODAL_X_OFFSET + MODAL_WIDTH - 1
+            ) {
+              return '|';
+            } else if (
+              i >= MODAL_X_OFFSET &&
+              i < MODAL_X_OFFSET + MODAL_WIDTH
+            ) {
+              return ' ';
+            }
+            return c;
+          })
+          .join('');
+      }
+
+      return line;
+    });
+
+    return result.join('\n');
+  };
+
+  const renderStringWithInfo = addInfoWindow(expandedRenderString);
+
   let output = '';
   output += `score: ${score}\n`;
   output += 'next:\n';
   output += nextPiece + '\n';
   output += '\n\n';
-  output += expandedRenderString;
+  output += renderStringWithInfo;
   const lines = output.split('\n');
   let longestLine = 0;
   lines.forEach(line => {
