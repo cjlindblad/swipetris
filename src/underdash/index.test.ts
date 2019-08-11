adff97ad0483cfd42a97a7fbf4e88a5700c7bd58
cj lindblad <carljohan.lindblad@gmail.com>
2019-08-11 16:28:50 +0200

    Added util for controlling string line lengths

--- src/underdash/index.test.ts
+++ src/underdash/index.test.ts
@@ -1,25 +1,48 @@
-import { isEmptyObject, wrapModulo } from './index';
+import { isEmptyObject, wrapModulo, maxLineLength } from './index';
 
-describe('underdash isEmptyObject', (): void => {
-  it('recognizes empty object', (): void => {
+describe('underdash isEmptyObject', () => {
+  it('recognizes empty object', () => {
     const isEmpty = isEmptyObject({});
     expect(isEmpty).toBe(true);
   });
 
-  it('dismisses non empty object', (): void => {
+  it('dismisses non empty object', () => {
     const isEmpty = isEmptyObject({ null: null });
     expect(isEmpty).toBe(false);
   });
 });
 
-describe('underdash wrapModulo', (): void => {
-  it('wraps around positive limit', (): void => {
+describe('underdash wrapModulo', () => {
+  it('wraps around positive limit', () => {
     const result = wrapModulo(5, 4);
     expect(result).toBe(1);
   });
 
-  it('wraps around negative limit', (): void => {
+  it('wraps around negative limit', () => {
     const result = wrapModulo(-1, 4);
     expect(result).toBe(3);
+  });
+});
+
+describe('underdash maxLineLength', () => {
+  it('breaks a long string into multiple lines', () => {
+    const string = 'this string is a bit too long';
+    const result = maxLineLength(string, 10);
+    const expectedResult = 'this\nstring is\na bit too\nlong';
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('keeps words with same width as limit', () => {
+    const string = 'lorem ipsum lorem ipsum';
+    const result = maxLineLength(string, 5);
+    const expectedResult = 'lorem\nipsum\nlorem\nipsum';
+    expect(result).toEqual(expectedResult);
+  });
+
+  it('handles words longer than limit', () => {
+    const string = 'one gargantually long word';
+    const result = maxLineLength(string, 4);
+    const expectedResult = 'one\ngar-\ngan-\ntua-\nlly\nlong\nword';
+    expect(result).toEqual(expectedResult);
   });
 });
--- src/underdash/index.ts
+++ src/underdash/index.ts
@@ -5,3 +5,46 @@ export const isEmptyObject = (obj: Object): boolean =>
   Object.entries(obj).length === 0 && obj.constructor === Object;
 
 export const wrapModulo = (x: number, n: number): number => ((x % n) + n) % n;
+
+// TODO refactor (has unit tests)
+export const maxLineLength = (str: string, maxLength: number): string => {
+  const words = str.match(/\S+/g) || [];
+
+  if (words.length === 0) {
+    return '';
+  }
+
+  const lines: string[] = [];
+  let nextLine = '';
+  words.forEach((word, index) => {
+    if (nextLine === '' && word.length <= maxLength) {
+      nextLine = word;
+    } else if (`${nextLine} ${word}`.length <= maxLength) {
+      nextLine = `${nextLine} ${word}`;
+    } else if (word.length > maxLength) {
+      if (nextLine.length >= maxLength - 1) {
+        lines.push(nextLine);
+        nextLine = '';
+      } else {
+        nextLine += ' ';
+      }
+      word.split('').forEach(char => {
+        if (`${nextLine}${char}`.length < maxLength) {
+          nextLine += char;
+        } else {
+          lines.push(`${nextLine}-`);
+          nextLine = char;
+        }
+      });
+    } else {
+      lines.push(nextLine);
+      nextLine = word;
+    }
+
+    if (index === words.length - 1) {
+      lines.push(nextLine);
+    }
+  });
+
+  return lines.join('\n');
+};
