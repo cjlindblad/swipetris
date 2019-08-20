07363fc80903bf323f6a1856723b6d73999c5bf7
cj lindblad <carljohan.lindblad@gmail.com>
2019-08-20 20:37:22 +0200

    Started porting web rendering to canvas

--- src/render/web.ts
+++ src/render/web.ts
@@ -3,17 +3,37 @@ import { Render } from './types';
 export const render: Render = param => {
   const { renderString, nextPiece, score } = param;
 
-  const gameboardElement = document.getElementById('input');
-  const scoreElement = document.getElementById('score');
-  const nextElement = document.getElementById('next');
+  const canvas = document.getElementById('canvas') as HTMLCanvasElement;
 
-  if (!gameboardElement || !scoreElement || !nextElement) {
-    throw new Error('Could not find game elements');
+  if (!canvas) {
+    throw new Error('Could not find canvas element');
   }
 
-  gameboardElement.innerText = renderString;
-  scoreElement.innerText = String(score);
-  nextElement.innerText = nextPiece;
+  const ctx = canvas.getContext('2d');
+
+  if (!ctx) {
+    throw new Error('Could not find context element');
+  }
+
+  const CANVAS_HEIGHT = window.innerHeight;
+  const CANVAS_WIDTH = CANVAS_HEIGHT / 2;
+
+  ctx.canvas.height = CANVAS_HEIGHT;
+  ctx.canvas.width = CANVAS_WIDTH;
+
+  ctx.fillStyle = 'rgba(255, 255, 255, 0.25)';
+  ctx.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+
+  const LINE_HEIGHT = 25;
+  const lines = renderString.split('\n');
+  let startY = 100;
+
+  ctx.fillStyle = 'rgb(0, 0, 0)';
+  ctx.font = '20px monospace';
+  lines.forEach(line => {
+    ctx.fillText(line, 50, startY);
+    startY += LINE_HEIGHT;
+  });
 };
 
 export default render;
