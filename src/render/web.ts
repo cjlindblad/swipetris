b2d1f4c44e2b5f6b7d557603a25914c45b2f9a9b
Carl-Johan Lindblad <cj.lindblad@bitio.se>
2019-08-22 17:09:49 +0200

    Have some random colors!

--- src/render/web.ts
+++ src/render/web.ts
@@ -4,11 +4,62 @@ import DependencyContainer from '../dependencyContainer';
 import { GAME_PIECE_TYPE } from '../gamePiece/enums';
 import { COLUMNS, ROWS } from '../config';
 
+// utils (move these out)
+const randomInt = (max: number): number =>
+  Math.floor(Math.random() * Math.floor(max));
+
+const createRandomColor = (): string => {
+  const r = randomInt(256);
+  const g = randomInt(256);
+  const b = randomInt(256);
+
+  return `rgb(${r}, ${g}, ${b})`;
+};
+
+const T_COLOR = createRandomColor();
+const L_COLOR = createRandomColor();
+const L_INVERTED_COLOR = createRandomColor();
+const S_COLOR = createRandomColor();
+const S_INVERTED_COLOR = createRandomColor();
+const I_COLOR = createRandomColor();
+const BLOCK_COLOR = createRandomColor();
+
 export const render: Render = param => {
   const gameCharSelector: GameCharSelector = DependencyContainer.resolve(
     'gameCharSelector'
   ) as GameCharSelector; // TODO should be automatic
+
+  // could be cleaner
   const EMPTY_SPACE_CHAR = gameCharSelector(GAME_PIECE_TYPE.EMPTY_SPACE);
+
+  const T = gameCharSelector(GAME_PIECE_TYPE.T);
+  const L = gameCharSelector(GAME_PIECE_TYPE.L);
+  const L_INVERTED = gameCharSelector(GAME_PIECE_TYPE.L_INVERTED);
+  const S = gameCharSelector(GAME_PIECE_TYPE.S);
+  const S_INVERTED = gameCharSelector(GAME_PIECE_TYPE.S_INVERTED);
+  const I = gameCharSelector(GAME_PIECE_TYPE.I);
+  const BLOCK = gameCharSelector(GAME_PIECE_TYPE.BLOCK);
+
+  const getPieceColor = (piece: string): string => {
+    switch (piece) {
+      case T:
+        return T_COLOR;
+      case L:
+        return L_COLOR;
+      case L_INVERTED:
+        return L_INVERTED_COLOR;
+      case S:
+        return S_COLOR;
+      case S_INVERTED:
+        return S_INVERTED_COLOR;
+      case I:
+        return I_COLOR;
+      case BLOCK:
+        return BLOCK_COLOR;
+      default:
+        return 'rgb(255, 255, 255)';
+    }
+  };
 
   const { renderString, gameBoard, nextPiece, score } = param;
 
@@ -44,11 +95,12 @@ export const render: Render = param => {
   const CELL_WIDTH = CANVAS_WIDTH / COLUMNS;
   const CELL_HEIGHT = CELL_WIDTH;
   if (gameBoard) {
-    ctx.fillStyle = 'rgb(255, 0, 0)';
     for (let y = 0; y < gameBoard.length; y++) {
       for (let x = 0; x < gameBoard[0].length; x++) {
         const cell = gameBoard[y][x];
         if (cell !== EMPTY_SPACE_CHAR) {
+          const color = getPieceColor(cell);
+          ctx.fillStyle = color;
           ctx.fillRect(
             INITIAL_X + x * CELL_WIDTH,
             INITIAL_Y + y * CELL_HEIGHT,
