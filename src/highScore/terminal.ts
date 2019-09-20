5fcb3f00485d4264c688c11ecf6a2ceaccb554ff
cj lindblad <carljohan.lindblad@gmail.com>
2019-09-20 23:04:56 +0200

    Terminal high score test rendering

--- src/highScore/terminal.ts
+++ src/highScore/terminal.ts
@@ -1,9 +1,14 @@
+const loadHighScore = (): number[] => {
+  return [13, 37, 13, 37];
+};
+
 const saveHighScore = (score: number): void => {
   // TODO not implemented
 };
 
 const highScore: HighScore = {
-  save: saveHighScore
+  save: saveHighScore,
+  load: loadHighScore
 };
 
 export default highScore;
